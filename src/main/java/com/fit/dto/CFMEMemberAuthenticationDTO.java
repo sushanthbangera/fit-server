d3f5bae1cfa3388cc1ef18fa4eae8793a23558f5
sushanthbangera <30630541+sushanthbangera@users.noreply.github.com>
2017-09-03 23:20:52 +0530

    Member Auth DTO
    DTO class for Member Authetication

---
+++ src/main/java/com/fit/dto/CFMEMemberAuthenticationDTO.java
@@ -0,0 +1,38 @@
+
+package com.fit.dto;
+
+/**
+ *
+ * @author Sushanth Bangera
+ */
+public class CFMEMemberAuthenticationDTO {
+    
+    private String contact;
+    private String password;
+    private String deviceid;
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDeviceid() {
+        return deviceid;
+    }
+
+    public void setDeviceid(String deviceid) {
+        this.deviceid = deviceid;
+    }
+    
+}
